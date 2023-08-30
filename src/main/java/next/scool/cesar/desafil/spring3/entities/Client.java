ac3995467f221ecc3fdf4b2d35534ae17b19a72d
petronio50 <petronioeoliveira@hotmail.com>
2023-08-29 21:02:21 -0300

    primeiro commit

---
+++ src/main/java/next/scool/cesar/desafil/spring3/entities/Client.java
@@ -0,0 +1,13 @@
+package next.scool.cesar.desafil.spring3.entities;
+
+public class Client {
+   private String name;
+   private int age;
+   private int dependents;
+   private double income;
+   private String material_status;
+   private Data createdAt;
+   private Data updateAt;
+    
+    
+}
---
+++ src/main/java/next/scool/cesar/desafil/spring3/entities/House.java
@@ -0,0 +1,9 @@
+package next.scool.cesar.desafil.spring3.entities;
+
+public class House {
+   private String brand;
+   private String model;
+   private int year;
+    
+    
+}
